1a6a519fd8507d5ef5c1b2e7f4dc7507d762b721
AdrianThePadrian <aruggia06@gmail.com>
2022-11-02 21:09:18 -0400

    Added Movement

---
+++ 3DFlappyBird/Assets/movement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    Rigidbody rb;
+    bool startMove = false;
+    public float speed = 0.2f;
+    public float jump = 10f;
+
+    bool canJump = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = jump * transform.up;
+            //if (canJump)
+            //{
+            //    canJump = false;
+            //    StartCoroutine(jumping());
+            //    StopCoroutine(jumping());
+            //}
+            startMove = true;
+            rb.useGravity = true;
+        }
+
+        if (startMove)
+        {
+            //rb.AddForce(gameObject.transform.forward * speed, ForceMode.Force);
+            transform.position = transform.position + transform.forward * speed;
+        }
+    }
+
+    IEnumerator jumping()
+    {
+
+        rb.velocity = jump * transform.up;
+        yield return new WaitForSeconds(0.5f);
+        canJump = true;
+    }
+}
